da97ab0bc008aebae8bbd3bcd276cba35665b85e
James King <james_andrew_king@hotmail.co.uk>
2021-07-29 15:06:49 -0700

    added mroe infomraive error messages to user sign up end point

--- routes/newUserRoutes.js
+++ routes/newUserRoutes.js
@@ -9,13 +9,26 @@ router
     .post(async (req, res) => {
         // *** LOOK INTO HOW TO SECURLEY STORE USER DATA ***
 
-        // Handle data on the client side to avoid excess API calls
+        // DATA to be passed from frontend: 
+        // {
+        //     "username": "HotGuy94",
+        //     "firstName": "James",
+        //     "lastName": "King",
+        //     "email": "test@test.com",
+        //     "password": "password"
+        // }
+
+        // Handle data validation on the client side to avoid excess API calls
         const rawUserData = req.body;
         
         try {
             //current DB entries to be updated as they use userName instead of correct username
             const data = await UserRecord.find({$or: [{email : rawUserData.email}, {username : rawUserData.username}]});
             // Differentiate between username and email
+            if (data[0].username === rawUserData.username && data[0].email === rawUserData.email) return res.status(409).send('a user with that email and username already exists');
+            if (data[0].email === rawUserData.email) return res.status(409).send('a user with that email already exists');
+            if (data[0].username === rawUserData.username) return res.status(409).send('a user with that username already exists');
+
             if (data.length !== 0) return res.status(409).send('a user with that email or username already exists');
         } catch (err) {
             console.log(err);
@@ -26,20 +39,12 @@ router
         newUserRecord.save(err => {
             if (err) {
                 console.log(err)
-                return res.send('error savng new record');
+                return res.status(502).send('error savng new record');
             }
             console.log('added new user record');
         })
 
-
-        // Add to DB
-
-
-        // If Succesfully added, send success resp
-
-        // If err, send err response
-
-        res.send('POST/ add new user');
+        res.status(201).send('new user added to DB');
     })
 
 module.exports = router;
