33144ed2daeb7abcc10fbf4d8f47bc4a2b1a4ad1
James King <james_andrew_king@hotmail.co.uk>
2021-07-30 12:13:53 -0700

    restructured error handling to only execute when a user is returned

--- routes/createUserRoutes.js
+++ routes/createUserRoutes.js
@@ -25,25 +25,27 @@ router
         
         try {
             const data = await UserRecord.find({$or: [{email : rawUserData.email}, {username : rawUserData.username}]});
-            if (data[0].username === rawUserData.username && data[0].email === rawUserData.email) return res.status(409).send('a user with that email and username already exists');
-            if (data[0].email === rawUserData.email) return res.status(409).send('a user with that email already exists');
-            if (data[0].username === rawUserData.username) return res.status(409).send('a user with that username already exists');
-            if (data.length !== 0) return res.status(409).send('a user with that email or username already exists');
+            console.log(data);
 
-            const newUserRecord = new UserRecord(createNewUser(rawUserData));
-        
-            // newUserRecord.save(err => {
-            //     if (err) {
-            //         console.log(err)
-            //         return res.status(502).send('error savng new record');
-            //     }
-            //     console.log('added new user record');
-            // })
+            if (data.length !== 0) {     
+                if (data[0].username === rawUserData.username && data[0].email === rawUserData.email) return res.status(409).send('a user with that email and username already exists');
+                if (data[0].email === rawUserData.email) return res.status(409).send('a user with that email already exists');
+                if (data[0].username === rawUserData.username) return res.status(409).send('a user with that username already exists');
+            }
+
+            const newUserRecord = new UserRecord(createNewUser(rawUserData));        
+            newUserRecord.save(err => {
+                if (err) {
+                    console.log(err)
+                    return res.status(502).send('error savng new record');
+                }
+                console.log('added new user record');
+            })
     
             res.status(201).send('new user added to DB');
         } catch (err) {
             console.log(err);
-            // Input on correct status code
+            // Add correct status code
             res.status(400).send(err);
         };
     })
