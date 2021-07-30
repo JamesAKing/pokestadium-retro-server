5e6a1ae8b058d1dead2ff9b98254f470ccc5e261
James King <james_andrew_king@hotmail.co.uk>
2021-07-29 18:02:13 -0700

    GET/ user record now retrieves user's record from DB

--- routes/userRecordRoutes.js
+++ routes/userRecordRoutes.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const UserRecord = require('../models/userRecord');
 const { authenticateJWT } = require('../utilities/authMiddleware');
 
 router
     .route('/')
-    .get(authenticateJWT, (req, res) => {
-
-        const playerId = req.user;
+    .get(authenticateJWT, async (req, res) => {
+        const { playerId } = req.user;
         
-        // Search User Record DB for a user that matches playerId
+        try {
+            const data = await UserRecord.find({ playerId : playerId});
+            
+            if (data.length === 0) return res.status(404).send('user data not present');
 
-        // return user data as object
-
-        res.send('GET/ User Record');
+            res.status(200).json(data[0]);
+        } catch (err) {
+            console.log(err);
+            res.status(500).send('error retrieving data');
+        }
     })
 
 module.exports = router;
